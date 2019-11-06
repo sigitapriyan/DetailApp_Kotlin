d5b2e23eb5a3c5ea5cdf2bb0992eeb0cc676b125
Sigit Apriyanur Rohim <sigitapriyanur@gmail.com>
2019-11-07 00:56:36 +0700

    Coba Pindah Gambar (Bug)

--- app/src/main/java/com/sigit/finalapp/DetailMerk.kt
+++ app/src/main/java/com/sigit/finalapp/DetailMerk.kt
@@ -1,9 +1,12 @@
 package com.sigit.finalapp
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_detail_merk.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,14 +19,19 @@ class DetailMerk : AppCompatActivity(), View.OnClickListener {
 
         val tvNamaMerk = findViewById<TextView>(R.id.tvNamaMerk)
         val tvDetailMerk = findViewById<TextView>(R.id.tvDetailMerk)
+        val imgMerk = findViewById<ImageView>(R.id.imgMerk)
 
-        val intent = intent
+        val extras = intent.extras
+        val b = extras!!.getByteArray("EXTRA_IMG")
+        val bmp = BitmapFactory.decodeByteArray(b, 0, b!!.size)
+
+
         val name = intent.getStringExtra("EXTRA_NAME")
         val desc = intent.getStringExtra("EXTRA_DESC")
 
         tvNamaMerk.text = name
         tvDetailMerk.text = desc
-
+        imgMerk.setImageBitmap(bmp)
     }
 
 
--- app/src/main/java/com/sigit/finalapp/MainActivity.kt
+++ app/src/main/java/com/sigit/finalapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.sigit.finalapp
 
 import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_detail_merk.*
 import kotlinx.android.synthetic.main.item_row_merk.*
+import java.io.ByteArrayOutputStream
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -45,9 +51,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val iniMerk = tv_item_merk.text.toString()
         val inidetailMerk = tv_item_detail.text.toString()
 
+        val iniGambar = BitmapFactory.decodeResource(resources, img_item_merk.id)
+        val stream = ByteArrayOutputStream()
+        iniGambar.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        val b = stream.toByteArray()
+
+
         val moveIntent = Intent(this@MainActivity, DetailMerk::class.java)
         moveIntent.putExtra("EXTRA_NAME", iniMerk)
         moveIntent.putExtra("EXTRA_DESC", inidetailMerk)
+        moveIntent.putExtra("EXTRA_IMG", b)
         startActivity(moveIntent)
 
     }
